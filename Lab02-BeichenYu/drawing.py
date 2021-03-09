0e18973654857559ac0faaa858b39865e765d2c6
Beichen Yu <polarisybc@qq.com>
2021-03-09 16:55:30 +0800

    Edit drawing.py

--- Lab02-BeichenYu/drawing.py
+++ Lab02-BeichenYu/drawing.py
@@ -3,15 +3,15 @@ from tkinter import messagebox
 
 window = tkinter.Tk()
 window.title('Drawing')
-window.geometry('600x400')
+window.geometry('1000x600')
 
 inputer = tkinter.Entry(window, width=5, show=None, highlightcolor='red', highlightthickness=1)
 inputer.pack()
 
-inputlabel = tkinter.Label(window, text='Please input a valid integer from 1 to 10', font='System')
+inputlabel = tkinter.Label(window, text='(Please input a valid positive integer)', font='System')
 inputlabel.pack()
 num = 0
-cv = tkinter.Canvas(window, bg='white')
+cv = tkinter.Canvas(window, bg='white', height=600 * 0.8, width=960)
 # cv.create_rectangle(10, 10, 110, 110)
 cv.pack()
 
@@ -33,33 +33,49 @@ def paintshortline(p1, p2):
     cv.create_line(shortline_point, fill='black', width=2)
 
 
+def paintlongline(p1, p2):
+    cv.create_line([p1, p2], fill='black', width=2)
+
+
 def create_line():
     global num
-
+    up = 20
+    down = 600 * 0.8 - 20
+    left = 20
+    right = 960 - 20
+    mid = (left + right) / 2
+    # paintlongline((left, 20), (right, 20))
+    # paintlongline((left, up), (left, down))
+    if num == 1:
+        paintlongline((left, (up + down) / 2), (right, (up + down) / 2))
+        return
     for i in range(1, num + 1):
-        line_point = [(20, 20 + 20 * i), (360, 20 + 20 * i)]
-        cv.create_line(line_point, fill='black', width=2)
-        if not i == 1:
-            shortlinenum = num - i + 1
-            if shortlinenum % 2 == 1:
-                p1 = (190, 20 * i)
-                p2 = (190, 20 + 20 * i)
+        dis = (down - up) / (num - 1)
+        left_point = (left, up + dis * (i - 1))
+        right_point = (right, up + dis * (i - 1))
+        paintlongline(left_point, right_point)
+        if i == num:
+            continue
+        shortlinenum = num - i
+        if shortlinenum % 2 == 1:
+            p1 = (mid, up + dis * (i - 1))
+            p2 = (mid, up + dis * i)
+            paintshortline(p1, p2)
+            for j in range(1, int(shortlinenum / 2 + 1)):
+                p1 = (mid - 2 * dis * j, up + dis * (i - 1))
+                p2 = (mid - 2 * dis * j, up + dis * i)
+                p3 = (mid + 2 * dis * j, up + dis * (i - 1))
+                p4 = (mid + 2 * dis * j, up + dis * i)
                 paintshortline(p1, p2)
-                for j in range(1, int(shortlinenum / 2 + 1)):
-                    p1 = (190 - 40 * j, 20 * i)
-                    p2 = (190 - 40 * j, 20 + 20 * i)
-                    p3 = (190 + 40 * j, 20 * i)
-                    p4 = (190 + 40 * j, 20 + 20 * i)
-                    paintshortline(p1, p2)
-                    paintshortline(p3, p4)
-            else:
-                for j in range(1, int(shortlinenum / 2 + 1)):
-                    p1 = (210 - 40 * j, 20 * i)
-                    p2 = (210 - 40 * j, 20 + 20 * i)
-                    p3 = (170 + 40 * j, 20 * i)
-                    p4 = (170 + 40 * j, 20 + 20 * i)
-                    paintshortline(p1, p2)
-                    paintshortline(p3, p4)
+                paintshortline(p3, p4)
+        else:
+            for j in range(1, int(shortlinenum / 2 + 1)):
+                p1 = (mid + dis - 2 * dis * j, up + dis * (i - 1))
+                p2 = (mid + dis - 2 * dis * j, up + dis * i)
+                p3 = (mid - dis + 2 * dis * j, up + dis * (i - 1))
+                p4 = (mid - dis + 2 * dis * j, up + dis * i)
+                paintshortline(p1, p2)
+                paintshortline(p3, p4)
 
 
 def catch_num():
@@ -71,16 +87,18 @@ def catch_num():
         return
     try:
         num = int(var)
-        if num > 10:
-            messagebox.showwarning('Warning', 'The number is too big!(Please input a valid integer from 1 to 10)')
-            inputer.delete(0, 'end')
-            catch_num()
-        elif num <= 0:
-            messagebox.showwarning('Warning', 'The number is too small!(Please input a valid integer from 1 to 10)')
+        # if num > 10:
+        #     messagebox.showwarning('Warning', 'The number is too big!(Please input a valid integer from 1 to 10)')
+        #     inputer.delete(0, 'end')
+        #     catch_num()
+        if num <= 0:
+            messagebox.showwarning('Warning',
+                                   'You can\'t input a negative number! Please input a valid positive integer')
             inputer.delete(0, 'end')
             catch_num()
     except:
-        messagebox.showwarning('Warning', 'Not a valid integer!(Please input a valid integer from 1 to 10)')
+        messagebox.showwarning('Warning',
+                               'Not a valid integer! Please input a valid positive integer')
         inputer.delete(0, 'end')
         catch_num()
     create_line()
